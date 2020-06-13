c64a33c990b46f64d52536ee8b316be462acc716
flaviosenne <flaviosenne123@gmail.com>
2020-06-13 09:22:35 -0300

    Adicionei as rotas

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,8 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AgendaComponent } from '../app/views/agenda/agenda.component'
+import {FuncionariosComponent} from '../app/views/funcionarios/funcionarios.component'
+import { LoginComponent} from '../app/views/login/login.component'
+import {PacienteComponent} from '../app/views/paciente/paciente.component'
+import {UsuarioComponent} from '../app/views/usuario/usuario.component'
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "usuario",
+    component: UsuarioComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'paciente',
+    component: PacienteComponent
+  },
+  {
+    path: 'funcionario',
+    component: FuncionariosComponent
+  },
+  {
+    path: 'agenda',
+    component: AgendaComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,11 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FuncionariosComponent } from './views/funcionarios/funcionarios.component';
+import { PacienteComponent } from './views/paciente/paciente.component';
+import { UsuarioComponent } from './views/usuario/usuario.component';
+import { LoginComponent } from './views/login/login.component';
+import { AgendaComponent } from './views/agenda/agenda.component';
+import { HomeComponent } from './views/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FuncionariosComponent,
+    PacienteComponent,
+    UsuarioComponent,
+    LoginComponent,
+    AgendaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
